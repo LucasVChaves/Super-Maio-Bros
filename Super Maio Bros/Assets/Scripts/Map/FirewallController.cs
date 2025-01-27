8056bc23a78d76f1fc8632dd431a15dfeebe5e4f
Lucas Chaves <lucasvchaves@gmail.com>
2025-01-27 09:11:38 -0300

    feat: Adiciona parede de fogo com velocidade variavel

---
+++ Super Maio Bros/Assets/Scripts/Map/FirewallController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FirewallController : MonoBehaviour {
+    public float moveSpeed = 0.12f;
+    public float speedIncrement = 0.02f;
+    public Vector2 startPos;
+    public Vector2 endPos;
+
+    private bool isMovingRight = true;
+    private int rightDirCount = 0;
+
+    void FixedUpdate() {
+        Vector3 targetPos = isMovingRight ? (Vector3)endPos : (Vector3)startPos;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed);
+    
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f) {
+            isMovingRight = !isMovingRight;
+            if (isMovingRight) {
+                rightDirCount++;
+                // A cada 5x que a parede vai pra direita ela aumentar a velocidade
+                if (rightDirCount % 5 == 0 && moveSpeed <= 1.2f) {
+                    moveSpeed += speedIncrement;
+                    Debug.Log("Firewall: Speed Increases");
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("-1 heart");
+            // TODO: Dar dano no player
+        }
+    }
+}
