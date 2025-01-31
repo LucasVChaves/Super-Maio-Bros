a24c86c3ef4f15547fefd6b21680f2d877f0e78e
Lucas Chaves <lucasvchaves@gmail.com>
2025-01-31 16:56:26 -0300

    feat: Adiciona preview da bomba e melhora instanciamento dela

--- Super Maio Bros/Assets/Scripts/Player/BombController.cs
+++ Super Maio Bros/Assets/Scripts/Player/BombController.cs
@@ -4,12 +4,16 @@ public class BombController : MonoBehaviour {
     public int maxBombCount = 5;
     public int currBombCount;
     public GameObject bombPrefab;
+    public GameObject bombPreviewPrefab;
     public Grid grid;
     public AudioClip placeBombSFX;
     private AudioSource audioSource;
+    private GameObject bombPreview;
     void Start() {
         currBombCount = maxBombCount;
         audioSource = GetComponent<AudioSource>();
+        bombPreview = Instantiate(bombPreviewPrefab);
+        bombPreview.SetActive(true);
     }
 
     void Update() {
@@ -18,15 +22,21 @@ public class BombController : MonoBehaviour {
             InstantiateBomb();
             audioSource.PlayOneShot(placeBombSFX, 1f);
         }
+        UpdateBombPreview();
     }
 
     void InstantiateBomb() {
-        /*
         Vector3Int cellPos = grid.WorldToCell(transform.position);
-        Vector3 spawnPos = grid.CellToWorld(cellPos);
+        Vector3 spawnPos = grid.GetCellCenterWorld(cellPos);
         Instantiate(bombPrefab, spawnPos, Quaternion.identity);
-        */
-        Instantiate(bombPrefab, transform.position, Quaternion.identity);
         currBombCount--;
+    }
+
+    void UpdateBombPreview() {
+        if (bombPreview == null) return;
+        
+        Vector3Int cellPos = grid.WorldToCell(transform.position);
+        Vector3 previewPos = grid.GetCellCenterWorld(cellPos);
+        bombPreview.transform.position = previewPos;
     }
 }
--- Super Maio Bros/Assets/Scripts/Player/PlayerHealth.cs
+++ Super Maio Bros/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,14 +7,19 @@ public class PlayerHealth : MonoBehaviour {
     public AudioClip hurtSFX;
     private int currHealth;
     private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
     void Start() {
         currHealth = maxHealth;
         audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     public void TakeDamage(int damage) {
         currHealth -= damage;
         audioSource.PlayOneShot(hurtSFX, 1f);
+        spriteRenderer.color = new Color(255, 0, 0, 255);
+        StartCoroutine(changeColorBack());
+
         Debug.Log("Player took damage. Current Health = " + currHealth);
 
         if (currHealth <= 0) {
@@ -25,5 +31,10 @@ public class PlayerHealth : MonoBehaviour {
         Debug.Log("Player Died");
         // Recarrega a cena, eu acho
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    IEnumerator changeColorBack() {
+        yield return new WaitForSeconds(0.75f);
+        spriteRenderer.color = new Color(255, 255, 255, 255);
     }
 }
